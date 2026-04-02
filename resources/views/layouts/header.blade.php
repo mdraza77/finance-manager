d7464df16f3f79466de985d55d661bda97661ac0
Md Raza <mdraza8397@gmail.com>
2026-04-02 11:00:27 +0530

    idebar icons fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -54,7 +54,7 @@
 
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
